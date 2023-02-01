14240e6aff516281b3c6626cc85ee25cf03d3853
Vladins3ki <123756024+Vladins3ki@users.noreply.github.com>
2023-02-01 13:57:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let list = [];
+
+
+document.getElementById("add").onclick = function(){
+    let word = document.getElementById("words").value;
+    list.push(word);
+    document.getElementById("words").value = "";
+
+  
+}
+
+document.getElementById("addSearch").onclick = function(){
+    let searchInput = document.getElementById("search").value;
+    let searchValue = (list.indexOf(searchInput) > -1);
+    if(searchValue == true){
+        document.getElementById("changeText").innerHTML = "The word you type in exists in the dictionary list!";
+    }else{
+        document.getElementById("changeText").innerHTML = "The word does not exist in the dictionary list!"
+    }
+    document.getElementById("search").value = "";
+    
+}
+
+
+
+
+
+
+
+
